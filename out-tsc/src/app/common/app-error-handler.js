827e2858199a1dafe03749f93593814aa418b2e1
Marcus Yu <marcus.yu@abbott.com>
2019-10-08 12:58:53 -0700

    Auto-generated commit

--- out-tsc/src/app/common/app-error-handler.js
+++
@@ -1,7 +0,0 @@
-export class AppErrorHandler {
-    handleError(error) {
-        alert('An unexpected error occurred.');
-        console.log(error);
-    }
-}
-//# sourceMappingURL=app-error-handler.js.map
